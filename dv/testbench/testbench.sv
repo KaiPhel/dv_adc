211530a48c908f2004f78463c423199b75ce01e3
jrafa2 <114171792+jrafa2@users.noreply.github.com>
2024-10-16 18:56:21 +0200

    init

---
+++ dv/testbench/testbench.sv
@@ -0,0 +1,27 @@
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+`include "dut_if.sv"
+`include "lib_test.sv"
+`include "adc_dms_model.sv"
+
+module top;
+  dut_if dut_if();
+  
+  //instantiate digital_top?
+  
+  //instantiate adc
+  
+  initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+    $shm_open("waves.shm");
+    $shm_probe("ASM");
+    //reset?
+  end
+  
+  initial begin
+    //interface to database
+    run_test(); //+UVM_TESTNAME=test_i2c_write
+  end
+    
+endmodule : top
